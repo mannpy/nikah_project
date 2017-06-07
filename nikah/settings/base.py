4fb77584e0ac4622de883714e450b26f2de0c26a
mannpy <mannpy@mannpy-K52F.(none)>
2017-06-07 18:52:46 +0500

    Settings, static files and templates

--- nikah/settings/base.py
+++ nikah/settings/base.py
@@ -16,9 +16,12 @@ from django.core.exceptions import ImproperlyConfigured
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+
+def rel_project(*x):
+    return os.path.abspath(os.path.join(BASE_DIR, *x))
 
 
 def get_env_variable(var_name):
@@ -61,7 +64,7 @@ ROOT_URLCONF = 'nikah.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [rel_project('templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    rel_project('static'),
+)
